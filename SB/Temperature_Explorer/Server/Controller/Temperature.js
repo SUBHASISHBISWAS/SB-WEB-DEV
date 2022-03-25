b442815712b527727f10f35c3f2bcb1b82cdc0b2
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2022-03-25 19:41:43 +0530

    Firebase Strated

--- SB/Temperature_Explorer/Server/Controller/Temperature.js
+++ SB/Temperature_Explorer/Server/Controller/Temperature.js
@@ -1,8 +1,11 @@
 const Post = require("../models/Temperature");
 
+const TemperatureDb = require("/Users/subhasish/Documents/APPLE/SUBHASISH/Development/GIT/Interstellar/SB-WEB-DEV/SB/Temperature_Explorer/Server/config");
+
 exports.getPosts = (req, res, next) => {
   //+ convert To Number
   console.log("Server Connected");
+  TemperatureDb.Add({ Temperature: 40 });
   return res
     .status(200)
     .json({ message: "Temperature Retrieved Successfully ", temperature: 30 });
