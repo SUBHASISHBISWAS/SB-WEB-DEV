0c08ecbc391c0551c50b9d75d5013e7701a919b1
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-12 10:39:17 +0530

    Repository is Registered with DI Container

--- ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Program.cs
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Program.cs
@@ -1,8 +1,16 @@
+using SubhasishsPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 app.UseStaticFiles();
 
