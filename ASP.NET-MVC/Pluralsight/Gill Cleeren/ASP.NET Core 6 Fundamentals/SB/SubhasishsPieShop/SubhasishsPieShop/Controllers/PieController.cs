d3dc1bf0187c6af5ca500ac90b56b9eca41bbad4
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-12 21:11:30 +0530

    Update PieController.cs

--- ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/PieController.cs
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/PieController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SubhasishsPieShop.Models;
 
 namespace SubhasishsPieShop.Controllers
 {
     public class PieController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository ?? throw new ArgumentNullException(nameof(pieRepository));
+            _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+
         public IActionResult Index()
         {
             return View();
