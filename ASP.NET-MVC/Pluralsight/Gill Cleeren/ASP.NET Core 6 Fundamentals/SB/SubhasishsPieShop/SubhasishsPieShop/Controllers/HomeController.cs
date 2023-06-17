2e500d7da3c6cfde6fee9abbebdd10f15f9ff0d6
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-17 19:43:40 +0530

    Demo: Adding Navigation to the Site -Added Home Controller and View

---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubhasishsPieShop.Models;
+using SubhasishsPieShop.ViewModels;
+
+namespace SubhasishsPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository ?? throw new ArgumentNullException(nameof(pieRepository));
+        }
+
+        public IActionResult Index()
+        {
+            var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(piesOfTheWeek);
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SubhasishsPieShop.Models;
+
+namespace SubhasishsPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek ?? throw new ArgumentNullException(nameof(piesOfTheWeek));
+        }
+    }
+}
