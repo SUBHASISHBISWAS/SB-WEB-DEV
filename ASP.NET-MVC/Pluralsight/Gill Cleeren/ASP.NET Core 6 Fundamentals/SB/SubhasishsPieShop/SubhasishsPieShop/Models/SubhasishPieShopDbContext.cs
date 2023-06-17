a17b35bd15da213184da68b1c7d20c539089f175
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-17 14:41:54 +0530

    Adding EF Core to the Application

---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Models/SubhasishPieShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SubhasishsPieShop.Models
+{
+    public class SubhasishPieShopDbContext:DbContext
+    {
+        public SubhasishPieShopDbContext(DbContextOptions<SubhasishPieShopDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
