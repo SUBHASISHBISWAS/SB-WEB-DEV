4ba22178060dd5c7b8000d3614e196b66c2d243f
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-18 15:19:03 +0530

    Added Category Menu and Popper.js

---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Components/CategoryMenu.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubhasishsPieShop.Models;
+
+namespace SubhasishsPieShop.Components
+{
+    public class CategoryMenu :ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository ?? throw new System.ArgumentNullException(nameof(categoryRepository));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.AllCategories.OrderBy(c=>c.CategoryName);
+            return View(categories);
+        }
+
+
+    }
+}
--- ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/PieController.cs
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/PieController.cs
@@ -16,12 +16,32 @@ namespace SubhasishsPieShop.Controllers
             _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
         }
 
-        public IActionResult PieList()
-        {
-            ViewBag.CurrentCategory = "";
+        //public IActionResult PieList()
+        //{
+        //    ViewBag.CurrentCategory = "";
 
-            var pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cake");
-            return View(pieListViewModel);
+        //    var pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese Cake");
+        //    return View(pieListViewModel);
+        //}
+
+        public IActionResult PieList(string category)
+        {
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All Pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.
+                    FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int pieId)
