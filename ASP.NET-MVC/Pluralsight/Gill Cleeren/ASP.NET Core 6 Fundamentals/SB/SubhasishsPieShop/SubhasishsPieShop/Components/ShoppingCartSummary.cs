b45556bb6a2adb16705a5393177db76d8913d0c4
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-18 14:24:40 +0530

    Demo: Creating View Components -ShoppingCart Added

---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubhasishsPieShop.Models;
+using SubhasishsPieShop.ViewModels;
+
+namespace SubhasishsPieShop.Components
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart ?? throw new System.ArgumentNullException(nameof(shoppingCart));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var _shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, 
+                _shoppingCart.GetShoppingCartTotal());
+            return View(_shoppingCartViewModel);
+        }
+
+    }   
+}
