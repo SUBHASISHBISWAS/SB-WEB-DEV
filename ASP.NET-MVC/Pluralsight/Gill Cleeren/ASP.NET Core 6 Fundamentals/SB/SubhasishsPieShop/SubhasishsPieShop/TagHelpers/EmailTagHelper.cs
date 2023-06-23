ad098a3a1fad7cbc3f819991d6bc4115a3f05432
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-23 12:30:13 +0530

    Creating a Custom Tag Helper

---
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SubhasishsPieShop.TagHelpers
+{
+    public class EmailTagHelper:TagHelper
+    {
+        public string Address { get; set; }
+        public string Content { get; set; }
+        override public void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+        }
+    }
+}
