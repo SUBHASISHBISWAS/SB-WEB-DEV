cc2be26026a8fd957bd72c80142ac8204662bdc3
SUBHASISH BISWAS <subhasishbiswas15@gmail.com>
2023-06-23 14:24:02 +0530

    Demo: Using Model Binding

--- ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/OrderController.cs
+++ ASP.NET-MVC/Pluralsight/Gill Cleeren/ASP.NET Core 6 Fundamentals/SB/SubhasishsPieShop/SubhasishsPieShop/Controllers/OrderController.cs
@@ -18,5 +18,31 @@ namespace SubhasishsPieShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
+            return View();
+        }
     }
 }
